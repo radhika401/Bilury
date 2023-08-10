41d0c2e7d3d6d9deda45f1f14db29de0db46760e
navyarn <navya.anugraphix@gmail.com>
2023-08-10 02:28:49 -0700

    commiting js code

---
+++ bilury.js
@@ -0,0 +1,26 @@
+function myFunction()
+{
+    var element = document.getElementById("bvid");
+    element.style.display="block";
+    
+}
+function bigImg(x) {
+    x.style.height = "200px";
+    x.style.width = "220px";
+  }
+  
+  function normalImg(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+  }
+
+  function bImg(x) {
+    x.style.height = "300px";
+    x.style.width = "360px";
+  }
+  
+  function nImg(x) {
+    x.style.height = "300px";
+    x.style.width = "360px";
+  }
+  
